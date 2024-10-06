13dae1f7f9b4273af85089c8234e4ce0fbab544b
JB Daulaus <daulaus@et.intechinfo.fr>
2024-10-07 00:23:22 +0200

    Task: add todo (only design) ✅

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,152 @@ import 'package:flutter/material.dart';
 import 'package:todo_list/todo.dart';
 
 void main() {
-  runApp(TodoListApp());
+  runApp(const TodoListApp());
 }
 
 class TodoListApp extends StatefulWidget {
-  TodoListApp({super.key});
+  const TodoListApp({super.key});
 
   @override
   State<TodoListApp> createState() => _TodoListAppState();
 }
 
 class _TodoListAppState extends State<TodoListApp> {
-  
-  List<Todo> todos = [
-    Todo(title: "Ceci est un titre", description: "Ceci est une description"),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainRoute(),
+    );
+  }
+}
+
+class MainRoute extends StatelessWidget {
+  MainRoute({
+    super.key,
+  });
+
+  final List<Todo> todos = [
+    Todo(title: "Ceci est un titre ", description: "Ceci est une description"),
     Todo(title: "Ceci est un deuxième titre", description: "Ceci est une deuxième description"),
   ];
 
   List<TodoListTile> buildTodoList() {
     List<TodoListTile> tiles = [];
-    for (Todo todo in todos) {
-      tiles.add(TodoListTile(title: todo.title, subtitle: todo.description));
+    for (var i = 0; i < todos.length; i++) {
+      Todo todo = todos[i];
+      todo.isCompleted = true;
+      tiles.add(TodoListTile(title: todo.title, subtitle: todo.description, todos: todos, todoIndex: i, ));
     }
     return tiles;
   }
 
+  void displayTodo(Todo todo) {
+    print("Titre: ${todo.title}, Description ${todo.description}, isCompleted: ${todo.isCompleted}, dueDate: ${todo.dueDate}");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: ListView(
-          children: buildTodoList(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tâches"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: buildTodoList(),
+              ),
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.add),
+        label: const Text("Créer une nouvelle tâche"),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CreateTodoRoute())
+          );
+        }
+      ),
+    );
+  }
+}
+
+class CreateTodoRoute extends StatefulWidget {
+  const CreateTodoRoute({
+    super.key,
+  });
+
+  @override
+  State<CreateTodoRoute> createState() => _CreateTodoRouteState();
+}
+
+class _CreateTodoRouteState extends State<CreateTodoRoute> {
+
+  final todoNameController = TextEditingController();
+  final todoDescriptionController = TextEditingController();
+  DateTime? todoDueDateValue;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Créer une nouvelle tâche"),
+      ),
+      body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: TextField(
+                controller: todoNameController,
+                decoration: const InputDecoration(
+                  hintText: "Tâche à réaliser"
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: TextField(
+                controller: todoDescriptionController,
+                decoration: const InputDecoration(
+                  fillColor: Colors.red,
+                  hintText: "Description de la tâche à réaliser"
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Text(
+                "Date d'échéance",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700
+                ),
+              ),
+            ),
+            CalendarDatePicker(
+              initialDate: DateTime.now(),
+              firstDate: DateTime.now(),
+              lastDate: DateTime(2100),
+              onDateChanged: (value) {
+                print(value);
+                todoDueDateValue = value;
+              }
+            ),
+            Center(
+              child: FloatingActionButton(
+                child: const Icon(Icons.add),
+                onPressed: () {
+                  print("Nouvelle tâche: ${todoNameController.value.text}, Description: ${todoDescriptionController.value.text}, Échéance: $todoDueDateValue");
+                }
+              ),
+            )
+          ],
+        ),
     );
   }
 }
@@ -42,11 +155,15 @@ class _TodoListAppState extends State<TodoListApp> {
 class TodoListTile extends StatefulWidget {
   final String title;
   final String subtitle;
+  final int todoIndex;
+  final List<Todo> todos;
 
   const TodoListTile({
     super.key,
     required this.title,
     required this.subtitle,
+    required this.todoIndex,
+    required this.todos
   });
 
   @override
@@ -54,31 +171,37 @@ class TodoListTile extends StatefulWidget {
 }
 
 class _TodoListTileState extends State<TodoListTile> {
-  bool isCompleted = false;
+
+  void displayTodo(Todo todo) {
+    print("Titre: ${todo.title}, Description ${todo.description}, isCompleted: ${todo.isCompleted}, dueDate: ${todo.dueDate}");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
+    Todo t = widget.todos[widget.todoIndex];
+    displayTodo(t);
+
+    return ListTile(      
       leading: Checkbox(
-        value: isCompleted,
+        value: widget.todos[widget.todoIndex].isCompleted,
         onChanged: (value) => {
           setState(() {
-           isCompleted = !isCompleted;
+           widget.todos[widget.todoIndex].isCompleted = !widget.todos[widget.todoIndex].isCompleted;
           })
         },
       ),
       title: Text(
         widget.title.toString(),
         style: TextStyle(
-          decoration: isCompleted ? TextDecoration.lineThrough : null,
-          color: isCompleted ? Colors.grey : null
+          decoration: widget.todos[widget.todoIndex].isCompleted ? TextDecoration.lineThrough : null,
+          color: widget.todos[widget.todoIndex].isCompleted ? Colors.grey : null
         ),
       ),
       subtitle: Text(
         widget.subtitle.toString(),
         style: TextStyle(
-          decoration: isCompleted ? TextDecoration.lineThrough : null,
-          color: isCompleted ? Colors.grey : null
+          decoration: widget.todos[widget.todoIndex].isCompleted ? TextDecoration.lineThrough : null,
+          color: widget.todos[widget.todoIndex].isCompleted ? Colors.grey : null
         ),
       ),
     );
